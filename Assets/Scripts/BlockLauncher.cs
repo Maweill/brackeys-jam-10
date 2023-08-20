d8eb96e2b3f9fed9881dd3937b67c953b12276b6
bednyakov <bednyakov99@gmail.com>
2023-08-20 21:32:34 +0300

    Исправил раскачивание и отпускание блока

--- Assets/Scripts/BlockLauncher.cs
+++ Assets/Scripts/BlockLauncher.cs
@@ -3,28 +3,47 @@ using UnityEngine;
 
 public class BlockLauncher : MonoBehaviour
 {
-	[SerializeField] private float _amplitudeX = 3f;
-	[SerializeField] private float _amplitudeY = 0.5f;
-	[SerializeField] private float _frequency = 2.0f;
-	[SerializeField] private Vector3 _startPosition = new(0, 0, 0);
+	[SerializeField] private float _maxAngle;
+	[SerializeField] private float _ropeLength;
+
+	private readonly Vector3 _ropeSplitPoint = new(0, 1, 0);
 
 	private BlockFactory _blockFactory;
 
 	private Rigidbody2D _currentBlock;
+	private Vector3 _blockPosition;
+	private Vector3 _blockRelativeStartPosition;
+	private Vector3 _previousBlockPosition;
+
+	private LineRenderer _mainRope;
+	private LineRenderer _sideRope1;
+	private LineRenderer _sideRope2;
+
 
 	private void Awake()
 	{
 		_blockFactory = GetComponent<BlockFactory>();
+		LineRenderer[] _lineRenderers = GetComponentsInChildren<LineRenderer>();
+
+		_mainRope = _lineRenderers[0];
+		_sideRope1 = _lineRenderers[1];
+		_sideRope2 = _lineRenderers[2];
+		_blockRelativeStartPosition = transform.position - new Vector3(0f, _ropeLength, 0f);
 	}
 
 	private void Start()
 	{
-		_currentBlock = _blockFactory.CreateBlock(_startPosition);
+		_currentBlock = _blockFactory.CreateBlock(_blockRelativeStartPosition);
 	}
 
 	private void Update()
 	{
+		if (_currentBlock) {
+			_previousBlockPosition = _currentBlock.transform.position;
+		}
+
 		MoveBlock();
+		UpdateRope();
 
 		if (Input.GetKeyDown(KeyCode.Space)) {
 			StartCoroutine(DropBlock());
@@ -33,30 +52,45 @@ public class BlockLauncher : MonoBehaviour
 
 	private void MoveBlock()
 	{
+		float angle = _maxAngle * Mathf.Sin(Mathf.Sqrt(9.81f / _ropeLength) * Time.time);
+		float xMovement = _blockRelativeStartPosition.x + _ropeLength * Mathf.Sin(angle * Mathf.Deg2Rad);
+
+		_blockPosition =
+			new Vector3(xMovement,
+			            _blockRelativeStartPosition.y - _ropeLength * Mathf.Cos(angle * Mathf.Deg2Rad) + _ropeLength,
+			            _blockRelativeStartPosition.z);
+
 		if (_currentBlock == null) {
 			return;
 		}
 
-		float yMovement = _amplitudeY * Mathf.Sin(_frequency * Time.time);
-		float xMovement = _amplitudeX * Mathf.Cos(_frequency * Time.time);
+		_currentBlock.transform.position = _blockPosition;
+	}
 
-		_currentBlock.transform.position =
-			new Vector3(_startPosition.x + xMovement, _startPosition.y + yMovement, _startPosition.z);
+	private void UpdateRope()
+	{
+		Vector3 mainRopeEnd = _blockPosition + _ropeSplitPoint;
+		_mainRope.SetPosition(0, transform.position);
+		_mainRope.SetPosition(1, mainRopeEnd);
+
+		_sideRope1.SetPosition(0, mainRopeEnd);
+		_sideRope1.SetPosition(1, _blockPosition + new Vector3(-0.5f, 0, 0));
+
+		_sideRope2.SetPosition(0, mainRopeEnd);
+		_sideRope2.SetPosition(1, _blockPosition + new Vector3(0.5f, 0, 0));
 	}
 
 	private IEnumerator DropBlock()
 	{
 		_currentBlock.isKinematic = false;
 
-		// Вычисляем производные
-		float yVelocity = _amplitudeY * _frequency * Mathf.Cos(_frequency * Time.time);
-		float xVelocity = -_amplitudeX * _frequency * Mathf.Sin(_frequency * Time.time);
+		Vector3 positionDifference = _currentBlock.transform.position - _previousBlockPosition;
+		float deltaTime = Time.deltaTime;
 
-		// Устанавливаем начальную скорость для блока
-		_currentBlock.velocity = new Vector2(xVelocity, yVelocity);
+		_currentBlock.velocity = new Vector2(positionDifference.x / deltaTime, positionDifference.y / deltaTime);
 
 		_currentBlock = null;
 		yield return new WaitForSeconds(1f);
-		_currentBlock = _blockFactory.CreateBlock(_startPosition);
+		_currentBlock = _blockFactory.CreateBlock(_blockPosition);
 	}
 }
