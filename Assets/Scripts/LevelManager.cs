5ba39d99fba5fd8f5d0ca7d7970ac81b0b456a3f
bednyakov <bednyakov99@gmail.com>
2023-08-22 18:08:40 +0300

    Собрал игру из сделанных частей #13

--- Assets/Scripts/LevelManager.cs
+++ Assets/Scripts/LevelManager.cs
@@ -1,36 +1,50 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.Serialization;
 
 public class LevelManager : MonoBehaviour
 {
-    private CameraController _cameraController;
+	private CameraController _cameraController;
 
-    private void Start()
-    {
-        _cameraController = FindObjectOfType<CameraController>();
+	private void Start()
+	{
+		_cameraController = FindObjectOfType<CameraController>();
 
-        if (_cameraController == null) 
-            Debug.LogWarning("LevelManager: CameraController не найден, _cameraController = null");
-        
-        _cameraController.OnCameraMovedDown += HandleCameraMovedDown;
-    }
-    
-    public void OnLevelCompleted()
-    {
-        if (_cameraController == null) return;
-        _cameraController.MoveCameraDown();
-    }
-    
-    private void HandleCameraMovedDown()
-    {
-        Debug.Log("Камера переместилась вниз.");
-    }
+		if (_cameraController == null) {
+			Debug.LogWarning("LevelManager: CameraController не найден, _cameraController = null");
+		}
 
-    private void OnDestroy()
-    {
-        if (_cameraController == null) return;
-        _cameraController.OnCameraMovedDown -= HandleCameraMovedDown;
-    }
+		_cameraController.OnCameraMovedDown += HandleCameraMovedDown;
+		GameEvents.GameStarted += OnGameStarted;
+	}
+
+	public void OnLevelCompleted()
+	{
+		if (_cameraController == null) {
+			return;
+		}
+
+		_cameraController.MoveCameraDown();
+	}
+	
+	private void OnGameStarted()
+	{
+		if (_cameraController == null) {
+			return;
+		}
+
+		_cameraController.MoveCameraDown();
+	}
+
+	private void HandleCameraMovedDown()
+	{
+		Debug.Log("Камера переместилась вниз.");
+	}
+
+	private void OnDestroy()
+	{
+		if (_cameraController == null) {
+			return;
+		}
+
+		_cameraController.OnCameraMovedDown -= HandleCameraMovedDown;
+	}
 }
