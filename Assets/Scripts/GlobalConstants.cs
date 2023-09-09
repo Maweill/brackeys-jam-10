3bad738b71ffc023f1e13f09f983e569d93b4e15
Daniel-Rue <78970069+Daniel-Rue@users.noreply.github.com>
2023-09-09 17:13:17 +0300

    Переделал систему подсчёта блоков в доме и исправил баги

--- Assets/Scripts/GlobalConstants.cs
+++ Assets/Scripts/GlobalConstants.cs
@@ -4,8 +4,9 @@ public class GlobalConstants : MonoBehaviour
 {
     public const float CAMERA_SHIFT_DURATION = 2f;
     public const float LEVEL_SHIFT = 10.1f;
-    public const float BLOCK_TEMPLATE_FILL_MIN_PERCENTAGE = 50f;
+    public const float BLOCK_TEMPLATE_FILL_MIN_PERCENTAGE = 30f;
     public const float BLOCK_GRAVITY = 9.81f;
     public const float MIN_FILLED_HOUSES_PERCENTAGE = 50f;
+    public const float MIN_FILLED_HOUSE_PERCENTAGE = 40f;
     public const int LEVEL_LIVES = 3;
 }
