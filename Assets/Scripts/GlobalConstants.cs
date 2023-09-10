043fd3d121430587a905ada06eb33e3b0ab47970
Daniel-Rue <78970069+Daniel-Rue@users.noreply.github.com>
2023-09-10 14:39:14 +0300

    Поправил левел дизайн

--- Assets/Scripts/GlobalConstants.cs
+++ Assets/Scripts/GlobalConstants.cs
@@ -4,7 +4,7 @@ public class GlobalConstants : MonoBehaviour
 {
     public const float CAMERA_SHIFT_DURATION = 2f;
     public const float LEVEL_SHIFT = 10.1f;
-    public const float BLOCK_TEMPLATE_FILL_MIN_PERCENTAGE = 30f;
+    public const float BLOCK_TEMPLATE_FILL_MIN_PERCENTAGE = 60f;
     public const float BLOCK_GRAVITY = 9.81f;
     public const float MIN_FILLED_HOUSES_PERCENTAGE = 50f;
     public const float MIN_FILLED_HOUSE_PERCENTAGE = 40f;
